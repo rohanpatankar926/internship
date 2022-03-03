0532949f0d9ddef7203622049c1bed6e7c62908c
Rohan Patankar <rohanpatankar926@gmail.com>
2022-03-04 01:36:19 +0530

    committed

--- app.py
+++ app.py
@@ -5,13 +5,15 @@ import joblib
 import numpy as np
 import yaml
 import pandas as pd
-
+import logging
+logging.basicConfig(format='%(message)s',level=logging.INFO)
 
 params_path="params.yaml"
 webapp_root="webapp"
 static_dir=os.path.join(webapp_root,"static")
 template_dir=os.path.join(webapp_root,"templates")
 
+logging.info("folder linking successful")
 
 app=Flask(__name__,static_folder=static_dir,template_folder=template_dir)
 
@@ -19,6 +21,8 @@ def read_params(config_path):
     with open(config_path) as yaml_file:
         config=yaml.safe_load(yaml_file)
     return config
+
+logging.info("parmas.yaml read successful...")
 
 def predict(data):
     config=read_params(params_path)
@@ -30,9 +34,19 @@ def predict(data):
 
 
 def api_response(request):
-    pass
+    try:
+        data=np.array([list(request.json.values())])
+        response=predict(data)
+        response={"response":response}
+        return jsonify(response)
+    except Exception as e:
+        print(e)
+        error={"something went wrong try again!!"}
+        return error
 
 data1=pd.read_csv("data_given\insurance_updated.csv")
+
+logging.info("csv read successful")
 
 @app.route("/",methods=["GET","POST"])
 def index():
@@ -52,8 +66,7 @@ def index():
                 children=request.form.get("children")
                 smoker=request.form.get("smoker")
                 region=request.form.get("region")
-                response=predict(pd.DataFrame([[age, sex, bmi, children, smoker, region]], 
-                columns=['age', 'sex', 'bmi', 'children', 'smoker', 'region']))
+                response=predict(pd.DataFrame([[age, sex, bmi, children, smoker, region]], columns=['age', 'sex', 'bmi', 'children', 'smoker', 'region']))
                 return render_template("index.html",response=str(response[0]))
             elif request.json:
                 response=api_response(request)
@@ -63,10 +76,10 @@ def index():
             error = {"error": "Something went wrong!! Try again later!"}
             error = {"error":e}
             return render_template("404.html", error=error)
-    else:
-        return render_template("index.html",sex=sex,smoker=smoker,region=region)
+    
+    return render_template("index.html",sex=sex,smoker=smoker,region=region)
 
-
+logging.info("application running succesfully")
 if __name__=="__main__":
     app.run(debug=True)
     
